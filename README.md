0d61804519a1970b0758a3415e9fc16c81411542
Yanan <Evazhao919@gmail.com>
2026-04-29 13:45:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# HDWorkshop-responsive
+HDWorkshop is a agency website. It's fully responsive. 
+# Technology used : 
+1. HTML 5
+2. CSS 3
+3. Javascript (vanilla)
+4. Jquery
+5. Bootstrap
+6. MixItUp plugin
+ 
+ ## live link : 
+https://IT company.github.io/HDWorkshop-responsive/
---
+++ asset/js/main.js
@@ -0,0 +1,37 @@
+$('#myCarousel').carousel({
+    interval: 5000
+})
+
+$('#myCarousel .carousel-item').each(function() {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
+var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
+
+    var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '[data-ref~="mixitup-target"]'
+    }
+});
+
+document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
